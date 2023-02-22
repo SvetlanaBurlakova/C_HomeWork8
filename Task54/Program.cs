5e698031fc27c1bcf299b8a16312a225029fc772
Alexey Burlakov <lexiphanx@gmail.com>
2023-02-22 13:52:07 +0400

    Comments on Task54

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -30,14 +30,16 @@ void PrintMatrix(int[,] matrix)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.Write($"{matrix[i,j]} ");
+            Console.Write($"{matrix[i,j]} "); // образуется лишний пробел в конце, но тут это не критично
         }
 
         Console.WriteLine();
     }
 }
-int [] arraySort(int[] array)
+// старайся добавлять пустые строки между функциями - хороший тон форматирования
+int [] arraySort(int[] array) // старайся придерживаться одинакового кейсинга названий методов - с большой буквы (тоже хороший тон)
 {
+// старайся внутри {} добавлять оффсет - тоже хороший тон
 for (int i = 0; i < array.Length - 1; i++)
 {
     int maxPosition = i;
@@ -45,20 +47,26 @@ for (int i = 0; i < array.Length - 1; i++)
     {
         if(array[j] > array[maxPosition]) maxPosition = j;
     }
+    // 1) чисто для информации, в C# вместо 3х следующих строк можно написать так:
+    //    (array[i], array[maxPosition]) = (array[maxPosition], array[i]);
+    // 2) если maxPosition остался равным i, то подмену производить не нужно
     int temporary = array[i];
     array[i] = array[maxPosition];
     array[maxPosition] = temporary;
 }
-return array;
+return array; // не имеет смысла возвращать массив, потому что ты и так меняешь содержимое переданного массива, а массиввы всегда by-reference передаются
 }
 int [,] SortByRowsMatrix(int [,] matrix)
 {
     int [,] result = new int[matrix.GetLength(0), matrix.GetLength(1)];
+    // если результат не зависит от направления, то всегда лучше в обратную сторону, например for (int i = matrix.GetLength(0) - 1; i >= 0; i--)
+    // потому что в таком варианте метод matrix.GetLength(0) вызовется только один раз (в начале цикла) а не в каждой итерации
+    // плюс сравнение с нулём всегда эффективнее чем сравнение с другим числом
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        int [] array=new int[matrix.GetLength(1)];
+        int [] array=new int[matrix.GetLength(1)]; // этот массив можно создать 1 раз до цикла, а не создавать внутри каждой итерации, он же перетирается полностью
         for (int j = 0; j < matrix.GetLength(1); j++)  array[j]=matrix[i,j];
-        array=arraySort(array);
+        array=arraySort(array); // вот тут заместо ручной сортировки можно написать Array.Sort(array, (a, b) => b.CompareTo(a));
         for (int j = 0; j < matrix.GetLength(1); j++)  result[i,j]=array[j];
     }
     return result;
